618fd7439bca270eb5b700b5c4004099120fc76f
Ekaterina <pechnikova.07@mail.ru>
2022-07-10 16:24:22 +0300

    Get Product Of Numbers Between M and M

---
+++ Task64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу,
+// которая выведет произведение натуральных чисел в промежутке от M до N.
+// M = 1; N = 5. ->120
+// M = 4; N = 6. -> 120
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+WriteLine();
+WriteLine(GetProd(m, n));
+WriteLine();
+
+int GetProd(int a, int b)
+{
+    return a <= b ? a * GetProd(a + 1, b) : 1;
+}
+
